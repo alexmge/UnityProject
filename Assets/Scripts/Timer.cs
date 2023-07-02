d073f0ae0157401b19d29b6700b2ca846e6bf5fd
adrien.aribaut-gaudin <adrien.aribaut-gaudin@epita.fr>
2023-07-02 22:33:04 +0200

    add: pause menu fonctionnel

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -34,11 +34,11 @@ public class Timer : MonoBehaviour
 
     void DisplayTime(float timeToDisplay)
     {
-        timeToDisplay += 1;
 
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
         timer_text.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeToDisplay += 1;
     }
 }
